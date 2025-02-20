49090ad108da710293731d5abbe0fced37ae75dd
Vishal Hari <vishalhari01@gmail.com>
2025-02-20 20:15:34 +0530

    first commit

---
+++ src/products.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+
+const Products = () => {
+  return (
+    <div>
+    <ProductCard title="Lenovo" price="20000" />
+    <ProductCard title="Lenovo" price="20000"  />
+    <ProductCard title="Lenovo" price="20000"  />
+    <ProductCard title="Lenovo" price="20000"  />
+    <ProductCard title="Lenovo" price="20000"  />
+    <ProductCard title="Lenovo" price="20000"  />
+    </div>
+  )
+}
+
+export default Products
