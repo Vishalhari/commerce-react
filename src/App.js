c6bd55b59e1d91ba6719edf0598d56a16130bf9d
Vishal Hari <vishalhari01@gmail.com>
2025-02-21 21:19:18 +0530

    pages design

--- src/App.js
+++ src/App.js
@@ -3,28 +3,45 @@ import './App.css';
 
 import Products from './components/Products/Products';
 import { useState } from 'react';
+import Header from './Pages/Header';
 
 function App() {
-  const [cart,setCart] = useState([]);
-  const addToCart = (product) => {
-    console.log(product);
-    setCart((prevCart) => {
-      const existingProduct = prevCart.find((item)=>item.id ===product.id);
-      if (existingProduct) {
-        return prevCart.map((item) => item.id ===product.id ? {...item,quantity:item.quantity+1}:item);
-      } else {
-        return [...prevCart,{product,quantity:1}];
-      }
-    })
-  }
+  
+  
 
-  const removefromCart = () => {
-
-  }
+  
   return (
-    <div className="App">
-     <Products addToCart={addToCart} removefromCart={removefromCart} />
+   <div>
+    <Header />
+    <div class="container-fluid pt-5">
+        <div class="row px-xl-5 pb-3">
+            <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                <div class="d-flex align-items-center border mb-4" style={{ padding: "30px" }}>
+                    <h1 class="fa fa-check text-primary m-0 mr-3"></h1>
+                    <h5 class="font-weight-semi-bold m-0">Quality Product</h5>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                <div class="d-flex align-items-center border mb-4" style={{ padding: "30px" }}>
+                    <h1 class="fa fa-shipping-fast text-primary m-0 mr-2"></h1>
+                    <h5 class="font-weight-semi-bold m-0">Free Shipping</h5>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                <div class="d-flex align-items-center border mb-4" style={{ padding: "30px" }}>
+                    <h1 class="fas fa-exchange-alt text-primary m-0 mr-3"></h1>
+                    <h5 class="font-weight-semi-bold m-0">14-Day Return</h5>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                <div class="d-flex align-items-center border mb-4" style={{ padding: "30px" }}>
+                    <h1 class="fa fa-phone-volume text-primary m-0 mr-3"></h1>
+                    <h5 class="font-weight-semi-bold m-0">24/7 Support</h5>
+                </div>
+            </div>
+        </div>
     </div>
+   </div>
   );
 }
 
